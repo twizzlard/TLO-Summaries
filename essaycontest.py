461778067f106700a81761edf98b60030467380c
twizzlard <76930650+twizzlard@users.noreply.github.com>
2023-08-14 20:03:30 -0400

    Update essaycontest.py

--- essaycontest.py
+++ essaycontest.py
@@ -1,12 +1,12 @@
 import openpyxl
 from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
 import zipfile
 import os
-import streamlit as st
 import tempfile
+import streamlit as st
 
-# Function to process the Excel file
 def process_excel(file):
     workbook = openpyxl.load_workbook(file)
     sheet = workbook.active
@@ -16,40 +16,33 @@ def process_excel(file):
     for idx, heading in enumerate(headings):
         assert sheet.cell(row=1, column=idx+1).value == heading, f"Expected {heading}, but found {sheet.cell(row=1, column=idx+1).value}"
 
-    # Temporary directory to store the PDFs
     with tempfile.TemporaryDirectory() as pdf_dir:
-        # Iterate through the rows, starting from the second row (indexing from 1)
         for row_idx in range(2, sheet.max_row + 1):
             index = sheet.cell(row=row_idx, column=1).value
             contents = [sheet.cell(row=row_idx, column=col_idx+1).value for col_idx in range(1, len(headings))]
 
-            # Create PDF file
             pdf_file = f"{pdf_dir}/Entry_{index}.pdf"
-            c = canvas.Canvas(pdf_file, pagesize=letter)
-            width, height = letter
-            
-            # Entry number
-            c.drawString(100, height - 80, f"Entry# {index}")
-            
-            # Add contents to the PDF
-            for idx, content in enumerate(contents):
-                c.drawString(100, height - 120 - (idx * 70), f"{headings[idx+1]}:")
-                c.drawString(100, height - 140 - (idx * 70), str(content))
+            doc = SimpleDocTemplate(pdf_file, pagesize=letter)
+            styles = getSampleStyleSheet()
+            styles["Normal"].fontSize = 10
+            styles["Normal"].leading = 12  # Line height
+            content_list = []
+            content_list.append(Paragraph(f"Entry# {index}", styles["Normal"]))
 
+            for idx, text_content in enumerate(contents):
+                content_list.append(Paragraph(f"{headings[idx+1]}:", styles["Normal"]))
+                content_list.append(Paragraph(str(text_content), styles["Normal"]))
 
-            # Add the scoring section
-            c.drawString(100, height - 320, "SCORING")
-            c.drawString(100, height - 340, "Research ______/30pts max")
-            c.drawString(100, height - 360, "Solution ______/40pts max")
-            c.drawString(100, height - 380, "Uniqueness ______/20pts max")
-            c.drawString(100, height - 400, "Professionalism ______/10pts max")
-            c.drawString(100, height - 420, "Total Score: ___________")
-            c.drawString(100, height - 440, "NOTES:")
+            content_list.append(Paragraph("SCORING", styles["Normal"]))
+            content_list.append(Paragraph("Research ______/30pts max", styles["Normal"]))
+            content_list.append(Paragraph("Solution ______/40pts max", styles["Normal"]))
+            content_list.append(Paragraph("Uniqueness ______/20pts max", styles["Normal"]))
+            content_list.append(Paragraph("Professionalism ______/10pts max", styles["Normal"]))
+            content_list.append(Paragraph("Total Score: ___________", styles["Normal"]))
+            content_list.append(Paragraph("NOTES:", styles["Normal"]))
 
-            # Save the PDF file
-            c.save()
+            doc.build(content_list)
 
-        # Create a ZIP file with the PDFs
         zip_file = tempfile.NamedTemporaryFile(delete=False, suffix=".zip")
         with zipfile.ZipFile(zip_file, 'w') as zipf:
             for root, _, files in os.walk(pdf_dir):
@@ -60,15 +53,13 @@ def process_excel(file):
 
 # Streamlit app
 st.title("Excel to PDF Converter")
-
 uploaded_file = st.file_uploader("Drag and drop your Excel file here:", type=["xlsx"])
 
 if uploaded_file is not None:
     with st.spinner("Processing..."):
         zip_path = process_excel(uploaded_file)
     st.success("Processing complete!")
-    
-    # Serve the ZIP file for download
+
     with open(zip_path, 'rb') as file:
         zip_bytes = file.read()
         st.download_button(
@@ -77,4 +68,3 @@ if uploaded_file is not None:
             file_name="entries.zip",
             mime="application/zip",
         )
-
